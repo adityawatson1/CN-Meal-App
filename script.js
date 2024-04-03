b03023b7eec42787166c66c52d59280a235ab1c7
Aditya <154841244+adityawatson1@users.noreply.github.com>
2024-04-04 00:55:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,19 @@ const fav =  document.getElementById('fav');
 const favoriteList = JSON.parse(localStorage.getItem('favoriteList')) || [];
 
 //taking value from user and fetch in api
-searchButton.addEventListener('click', () => {
+searchInput.addEventListener('keyup', () => {
   const searchQuery = searchInput.value;
 
-  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`)
+  setTimeout(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`)
     .then(response => response.json())
     .then(data => {
       if (data.meals === null) {
-        alert('No meals found for this search query.');
+          searchResult.innerHTML = '';
+          const noFavMessage = document.createElement('p');
+          noFavMessage.innerText = 'No meals found of your Search.';
+          searchResult.appendChild(noFavMessage);
+          return; 
       } else {
         showResult(data.meals);
       }
@@ -22,10 +27,12 @@ searchButton.addEventListener('click', () => {
     .catch(error => {
       console.error(error);
     });
+  }, 1000);
 });
 
 // Function to display meal items on screen
 function showResult(meals) {
+
   searchResult.innerHTML = '';
  
   
@@ -67,11 +74,16 @@ function showResult(meals) {
 
 // Function to add meals in favorites
 function addToFavorites(meal) {
+  // Check if the meal is not already in the favoriteList
   if (!favoriteList.some(favMeal => favMeal.idMeal === meal.idMeal)) {
+     // If the meal is not already in favorites, add it
     favoriteList.push(meal);
+    // Update the favoriteList in local storage
     localStorage.setItem('favoriteList', JSON.stringify(favoriteList));
+    // Display an alert indicating the meal has been added to favorites
     alert(`${meal.strMeal} has been added to your favorites.`);
   } else {
+     // If the meal is already in favorites, display an alert
     alert(`${meal.strMeal} is already in your favorites.`);
   }
 }
@@ -114,11 +126,17 @@ function showFav() {
 
 // Define a function to remove meal from fav
 function removeFromFavorites(meal) {
+  // Finding the index of the meal in the favoriteList array
   const mealIndex = favoriteList.findIndex(favMeal => favMeal.idMeal === meal.idMeal);
+   // Removing the meal from the favoriteList array
   if (mealIndex !== -1) {
+    // Removing the meal from the favoriteList array
     favoriteList.splice(mealIndex, 1);
+     // Updating the favoriteList in the local storage
     localStorage.setItem('favoriteList', JSON.stringify(favoriteList));
+    // Updating the UI to reflect the changes
     showFav();
+     // Displaying an alert to notify the user about the removal
     alert(`${meal.strMeal} has been removed from your favorites.`);
   }
 }
